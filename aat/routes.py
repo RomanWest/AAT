329e1733a1835f96f8214a5a2b8b93f3aa0c413a
Theodor Kozlowski <kozlowskitr@cardiff.ac.uk>
2021-03-28 16:05:45 +0100

    mock up

---
+++ aat/routes.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from flask import render_template, url_for, request, redirect, flash, g, current_app, session
+from aat import app, db
+from aat.models import User
+from aat.forms import RegistrationForm, LoginForm
+from flask_login import login_user, logout_user, login_required, current_user
+
+@app.route("/") 
+@app.route("/home") 
+def home():
+    return render_template('home.html')
+
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    user = User(username=form.username.data, first_name=form.first_name.data, last_name=form.last_name.data, email=form.email.data, password=form.password.data)
+    db.session.add(user)
+    db.session.commit()
+    flash('Registration successful!')
+    return redirect(url_for('home'))
+  return render_template('register.html',title='Register',form=form)
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+  form = LoginForm()
+  if form.validate_on_submit():
+    user = User.query.filter_by(email=form.email.data).first()
+    if user is not None and user.verify_password(form.password.data):
+        login_user(user)
+        flash('Login successful!')
+        return redirect(url_for('home'))
+    else:
+        flash("Email or password is incorrect")
+  return render_template('login.html',title='Login',form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    flash('Logout successful!')
+    return redirect(url_for('home'))
