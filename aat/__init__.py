33963bb4d6b14d5d22a6678613d54f54a9d23c78
Tim Williams <williamst38@cardiff.ac.uk>
2021-03-29 15:32:06 +0100

    reg works lmoa

--- aat/__init__.py
+++ aat/__init__.py
@@ -5,6 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__) 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SECRET_KEY'] = 'd530f8e038ee2524ec69e2f37bceeb44eb14473aa2853aeb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -15,6 +16,9 @@ from aat import routes
 
 from flask_admin import Admin
 from aat.views import AdminView
+from flask_admin.contrib.sqla import ModelView
 from aat.models import User
+
 admin = Admin(app,name='Admin panel',template_mode='bootstrap3')
-admin.add_view(AdminView(User, db.session))
+# admin.add_view(AdminView(User, db.session))
+admin.add_view(ModelView(User, db.session))
