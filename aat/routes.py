f5210a2cf17f3b74f580a7973765ccdd32fba118
Tim Williams <williamst38@cardiff.ac.uk>
2021-05-04 15:16:24 +0100

    stylecss

--- aat/routes.py
+++ aat/routes.py
@@ -58,9 +58,7 @@ filleditroute()
 
 @app.route("/Create-Formative")
 def createFormative():
-    multiple_all = Multiple.query.all()
-    print(multiple_all)
-    return render_template('Create Formative.html', multiple_all=multiple_all)
+    return render_template('Create Formative.html')
 
 @app.route("/Student-View-Progress")
 def studentViewProgress():
