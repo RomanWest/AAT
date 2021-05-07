92d5ad9e1c890a8d10231acfdf6b3535b871e7b1
Mike <64697794+M-Chapman@users.noreply.github.com>
2021-05-07 14:17:20 +0100

    added unique question and checkbox ID

--- aat/static/script/script.js
+++ aat/static/script/script.js
@@ -1,39 +1,54 @@
 // code to change title
-if (window.location.pathname == "/"){
-    document.getElementById("maintitle").innerHTML = "LOGIN"
+if (window.location.pathname == "/") {
+  document.getElementById("maintitle").innerHTML = "LOGIN"
 }
 
 //IMPORTANT
 // as long as all the names of the routes are in the format student-home or staff-home, the main title will be correct
 else {
-    document.getElementById("maintitle").innerHTML = window.location.pathname.split("/")[1].toUpperCase().replace(/-/g, " ");
+  document.getElementById("maintitle").innerHTML = window.location.pathname.split("/")[1].toUpperCase().replace(/-/g, " ");
 }
 
 //code to make the x work on flash messages
 var close = document.getElementsByClassName("close");
-    
+
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function(){
+  close[i].onclick = function () {
     var div = this.parentElement;
     div.style.opacity = "0";
-    setTimeout(function(){ div.style.display = "none"; });
+    setTimeout(function () { div.style.display = "none"; });
   }
 }
 
 //code to hide the home button if user is not logged in and on the login page
 
-if (window.location.pathname == "/login" || window.location.pathname == "/"){
- document.getElementById("homebtn").style.display= "none";
+if (window.location.pathname == "/login" || window.location.pathname == "/") {
+  document.getElementById("homebtn").style.display = "none";
 }
 
 
 //code to replace left margin with sidebar on student home
 //i changed == to includes so that itll work when we add additional routes after e.g student-home/stats
-if (window.location.pathname.includes("/Student-Home")){
-  document.getElementsById("content").style.marginLeft= "0";
+if (window.location.pathname.includes("/Student-Home")) {
+  document.getElementsById("content").style.marginLeft = "0";
 }
-else{
-  document.getElementById("content").style.marginLeft= "10%";
+else {
+  document.getElementById("content").style.marginLeft = "10%";
 }
 
+var count = 0;
+function questionCheck() {
+  var noElements = document.getElementsByClassName("checkbx");
+  for (var i = 0; i < noElements.length; i++) {
+    console.log("questionChecker",i);
+    if (document.getElementById("questionChecker",i).checked == true) {
+      count += 1;
+      console.log(count);
+    }
+    else {
+      count -= 1;
+      console.log(count);
+    }
+  }
+}
