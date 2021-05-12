12cf71c7ad1cd7e7f619303d9778e53d307d476d
Theodor Kozlowski <kozlowskitr@cardiff.ac.uk>
2021-05-12 15:54:34 +0100

    attempt assessment fields added

--- aat/routes.py
+++ aat/routes.py
@@ -55,11 +55,22 @@ def logout():
 fillblankroute()
 filleditroute()
 
+MultipleRoute()
+MultipleEditRoute()
+
+testassess_route()
+
 @app.route("/Create-Formative")
 def createFormative():
     multiple_all = Multiple.query.all()
     fill_all = Fill.query.all()
     return render_template('Create Formative.html', multiple_all=multiple_all, fill_all=fill_all)
+
+@app.route("/Create-Summative")
+def createSummative():
+    multiple_all = Multiple.query.all()
+    fill_all = Fill.query.all()
+    return render_template('Create Summative.html', multiple_all=multiple_all, fill_all=fill_all)
 
 @app.route("/Student-View-Progress")
 def studentViewProgress():
@@ -90,18 +101,11 @@ def viewCohort():
 def viewStudent():
     return render_template('View Student.html')
 
-@app.route("/Create-Summative")
-def createSummative():
-    multiple_all = Multiple.query.all()
-    fill_all = Fill.query.all()
-    return render_template('Create Summative.html', multiple_all=multiple_all, fill_all=fill_all)
 
 @app.route("/Create-Question")
 def createQuestion():
     return render_template('Create Question.html')
 
-MultipleRoute()
-MultipleEditRoute()
 
 @app.route("/Preview-Assessment")
 def previewAssessment():
@@ -110,48 +114,3 @@ def previewAssessment():
 @app.route("/Submit-Assessment")
 def submitAssessment():
     return render_template('Submit Assessment.html')
-
-testassess_route()
-
-# @app.route("/Attempt-Assessment/<int:assessment_id>",methods=['GET','POST'])
-# def testassess(assessment_id):
-#     assessment = Assessment.query.get_or_404(assessment_id)
-#     multiple_all = Multiple.query.all()
-#     fill_all = Fill.query.all()
-#     # questionQuery1 = (db.session.query(Assessment)
-#     #     .join(Multiple)
-#     #     .filter(Multiple.id.in_(Assessment.id)))
-        
-#         # .filter_by(id=Assessment.q1_id).all())
-    
-# #     if Assessment.q1_type == 'Multiple':
-# #         #looking_for = 'Multiple'
-# #         questionQuery1 = db.session.query(Assessment, Multiple).join(Multiple).filter_by(Multiple.id)
-# #         # questionQuery1 = (db.session.query(Multiple)
-# #         #     .join(Assessment)
-# #         #     .filter(Assessment.q1_id.in_(Multiple.id)))
-# #     else:
-# #         #looking_for = 'Fill'
-# #         # questionQuery1 = (db.session.query(Fill)
-# #         #     .join(Assessment)
-# #         #     .filter(Assessment.q1_id.in_(Fill.id)))
-# #         questionQuery1 = db.session.query(Assessment, Fill).join(Fill).filter_by(Fill.id)
-
-
-# #    # if looking_for 
-
-# #   #  Assessment.query.filter(Assessment.q1_type.contains(looking_for))
-
-# #     print(questionQuery1)
-    
-#     # for ( i < length of assessment):
-#     #     questions.append ( q1_type.query.filter_by (q1_id) )
-
-#     # q1_type = Assessment.query.get_or_404(q1_type)
-#     # q1_id = Assessment.query.get_or_404(q1_id)
-#     # q2_type = Assessment.query.get_or_404(q2_type)
-#     # q2_id = Assessment.query.get_or_404(q2_id)
-#     # q3_type = Assessment.query.get_or_404(q3_type)
-#     # q3_id = Assessment.query.get_or_404(q3_id)  
-#     print(assessment)
-#     return render_template('testassess.html', assessment=assessment, multiple_all=multiple_all, fill_all=fill_all)
