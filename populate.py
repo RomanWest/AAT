9e33122b2899963ff9839263bbf94a1b90d28cf6
Mike <64697794+M-Chapman@users.noreply.github.com>
2021-05-14 14:12:40 +0100

    added questions

--- populate.py
+++ populate.py
@@ -8,34 +8,34 @@ db.session.commit()
 # Multiple Fruit Med Formatvie
 # Multiple Fruit Hard Formative
 # Multiple Fruit Easy Summative
-db.session.add(Multiple(question="What colour is a banana?", correct="yellow", module_code="Fruit123",
-                        incorrect_1="blue", incorrect_2="red", incorrect_3="purple", difficulty="easy", is_summative=True, feedback="Refer to Fruit123 module!"))
+db.session.add(Multiple(question="What colour is a banana?", correct="yellow", module_code="Fruit123", incorrect_1="blue",
+               incorrect_2="red", incorrect_3="purple", difficulty="easy", is_summative=True, feedback="Refer to Fruit123 module!"))
 db.session.commit()
-db.session.add(Multiple(question="Which of the following is a citric fruit?", correct="orange", module_code="Fruit123",
-                        incorrect_1="apple", incorrect_2="pear", incorrect_3="kiwi", difficulty="easy", is_summative=True, feedback="Refer to Fruit123 module!"))
+db.session.add(Multiple(question="Which of the following is a citric fruit?", correct="orange", module_code="Fruit123", incorrect_1="apple",
+               incorrect_2="pear", incorrect_3="kiwi", difficulty="easy", is_summative=True, feedback="Refer to Fruit123 module!"))
 db.session.commit()
-db.session.add(Multiple(question="Where do bananas grow?", correct="a tree", module_code="Fruit123",
-                        incorrect_1="underground", incorrect_2="in caves", incorrect_3="in a lab", difficulty="easy", is_summative=True, feedback="Refer to Fruit123 module!"))
+db.session.add(Multiple(question="Where do bananas grow?", correct="a tree", module_code="Fruit123", incorrect_1="underground",
+               incorrect_2="in caves", incorrect_3="in a lab", difficulty="easy", is_summative=True, feedback="Refer to Fruit123 module!"))
 db.session.commit()
 # Multiple Fruit Medium Summative
-db.session.add(Multiple(question="apples are what colour?", correct="red", module_code="Fruit123",
-                        incorrect_1="black", incorrect_2="blue", incorrect_3="purple", difficulty="medium", is_summative=True, feedback="Refer to Fruit123 module!"))
+db.session.add(Multiple(question="apples are what colour?", correct="red", module_code="Fruit123", incorrect_1="black",
+               incorrect_2="blue", incorrect_3="purple", difficulty="medium", is_summative=True, feedback="Refer to Fruit123 module!"))
 db.session.commit()
-db.session.add(Multiple(question="Which of the following is not a fruit?", correct="potato", module_code="Fruit123",
-                        incorrect_1="apple", incorrect_2="grape", incorrect_3="lemon", difficulty="medium", is_summative=True, feedback="Refer to Fruit123 module!"))
+db.session.add(Multiple(question="Which of the following is not a fruit?", correct="potato", module_code="Fruit123", incorrect_1="apple",
+               incorrect_2="grape", incorrect_3="lemon", difficulty="medium", is_summative=True, feedback="Refer to Fruit123 module!"))
 db.session.commit()
-db.session.add(Multiple(question="What fruit is similar to a lemon?", correct="lime", module_code="Fruit123",
-                        incorrect_1="banana", incorrect_2="apple", incorrect_3="aubergine", difficulty="medium", is_summative=True, feedback="Refer to Fruit123 module!"))
+db.session.add(Multiple(question="What fruit is similar to a lemon?", correct="lime", module_code="Fruit123", incorrect_1="banana",
+               incorrect_2="apple", incorrect_3="aubergine", difficulty="medium", is_summative=True, feedback="Refer to Fruit123 module!"))
 db.session.commit()
 # Multiple Fruit Hard Summative
-db.session.add(Multiple(question="What fruit has a similar texture to potato?", correct="apple", module_code="Fruit123",
-                        incorrect_1="mango", incorrect_2="avocado", incorrect_3="orange", difficulty="hard", is_summative=True, feedback="Refer to Fruit123 module!"))
+db.session.add(Multiple(question="What fruit has a similar texture to potato?", correct="apple", module_code="Fruit123", incorrect_1="mango",
+               incorrect_2="avocado", incorrect_3="orange", difficulty="hard", is_summative=True, feedback="Refer to Fruit123 module!"))
 db.session.commit()
-db.session.add(Multiple(question="what is an apple?", correct="fruit", module_code="Fruit123",
-                        incorrect_1="vegetable", incorrect_2="herb", incorrect_3="cushion", difficulty="hard", is_summative=True, feedback="Refer to Fruit123 module!"))
+db.session.add(Multiple(question="what is an apple?", correct="fruit", module_code="Fruit123", incorrect_1="vegetable",
+               incorrect_2="herb", incorrect_3="cushion", difficulty="hard", is_summative=True, feedback="Refer to Fruit123 module!"))
 db.session.commit()
-db.session.add(Multiple(question="Where do apples grow?", correct="everywhere", module_code="Fruit123",
-                        incorrect_1="Europe", incorrect_2="Africa", incorrect_3="Asia", difficulty="hard", is_summative=True, feedback="Refer to Fruit123 module!"))
+db.session.add(Multiple(question="Where do apples grow?", correct="everywhere", module_code="Fruit123", incorrect_1="Europe",
+               incorrect_2="Africa", incorrect_3="Asia", difficulty="hard", is_summative=True, feedback="Refer to Fruit123 module!"))
 
 db.session.commit()
 # Multiple Biology Easy Formative
@@ -79,10 +79,27 @@ db.session.add(Multiple(question="Which of the following animals does not have a
                         incorrect_1="Cat", incorrect_2="Dog", incorrect_3="Sheep", difficulty="easy", is_summative=True, feedback="Refer to Biology123 module!"))
 db.session.commit()
 # Multiple Biology Med Summative
+db.session.add(Multiple(question="How many bones are in the human body?", correct="Two Hundred and Six", module_code="Biology123",
+                        incorrect_1="One hundred and Ninety Four", incorrect_2="Twnety Six", incorrect_3="Two Hundred and Eighty Five", difficulty="medium", is_summative=True, feedback="Refer to Biology123 module!"))
+db.session.commit()
+db.session.add(Multiple(question="Which of the following is not commonly a domestic pet?", correct="Hippopotamus", module_code="Biology123",
+                        incorrect_1="Cat", incorrect_2="Dog", incorrect_3="Rat", difficulty="medium", is_summative=True, feedback="Refer to Biology123 module!"))
+db.session.commit()
+db.session.add(Multiple(question="Which of the following animals does not have a tail?", correct="Chimpanze", module_code="Biology123",
+                        incorrect_1="Cat", incorrect_2="Dog", incorrect_3="Sheep", difficulty="medium", is_summative=True, feedback="Refer to Biology123 module!"))
+db.session.commit()
 # Multiple Biology Hard Summative
+db.session.add(Multiple(question="Which of the following animals does not have a tail?", correct="Chimpanze", module_code="Biology123",
+                        incorrect_1="Cat", incorrect_2="Dog", incorrect_3="Sheep", difficulty="hard", is_summative=True, feedback="Refer to Biology123 module!"))
+db.session.commit()
+db.session.add(Multiple(question="What animal spawns tadpoles?", correct="Frogs", module_code="Biology123",
+                        incorrect_1="Donkeys", incorrect_2="Mice", incorrect_3="Elephants", difficulty="hard", is_summative=True, feedback="Refer to Biology123 module!"))
+db.session.commit()
+db.session.add(Multiple(question="Which domestic animal is the best?", correct="Cat", module_code="Biology123",
+                        incorrect_1="Dog", incorrect_2="Donkey", incorrect_3="Lizard", difficulty="hard", is_summative=True, feedback="Refer to Biology123 module!"))
+db.session.commit()
 
 # Multiple Maths Easy Formative
-
 db.session.add(Multiple(question="How many sides does a square have?", correct="Four", module_code="Maths123",
                         incorrect_1="Three", incorrect_2="Two", incorrect_3="One", difficulty="easy", is_summative=False, feedback="Refer to Maths123 module!"))
 db.session.commit()
@@ -113,7 +130,25 @@ db.session.add(Multiple(question="What maths topic revolves around right handed 
                         incorrect_1="Statistics", incorrect_2="Simultaneous Equations", incorrect_3="Differentiation", difficulty="hard", is_summative=False, feedback="Refer to Maths123 module!"))
 db.session.commit()
 # Multiple Maths Easy Summative
+db.session.add(Multiple(question="x * 2 =", correct="2x", module_code="Maths123",
+                        incorrect_1="x", incorrect_2="y", incorrect_3="x^2", difficulty="easy", is_summative=False, feedback="Refer to Maths123 module!"))
+db.session.commit()
+db.session.add(Multiple(question="100 + 50 =", correct="150", module_code="Maths123",
+                        incorrect_1="100", incorrect_2="50", incorrect_3="500", difficulty="easy", is_summative=False, feedback="Refer to Maths123 module!"))
+db.session.commit()
+db.session.add(Multiple(question="5 * 2 =", correct="10", module_code="Maths123",
+                        incorrect_1="5", incorrect_2="100", incorrect_3="20", difficulty="easy", is_summative=False, feedback="Refer to Maths123 module!"))
+db.session.commit()
 # Multiple Maths Med Summative
+db.session.add(Multiple(question="500 - - 20 =", correct="520", module_code="Maths123",
+                        incorrect_1="480", incorrect_2="500", incorrect_3="50", difficulty="medium", is_summative=False, feedback="Refer to Maths123 module!"))
+db.session.commit()
+db.session.add(Multiple(question="3 * 5 + 2 =", correct="17", module_code="Maths123",
+                        incorrect_1="21", incorrect_2="7", incorrect_3="15", difficulty="medium", is_summative=False, feedback="Refer to Maths123 module!"))
+db.session.commit()
+db.session.add(Multiple(question="What category does trigonometry belong to?", correct="Calculus", module_code="Maths123",
+                        incorrect_1="Statistics", incorrect_2="Mechanics", incorrect_3="Differentiation", difficulty="medium", is_summative=False, feedback="Refer to Maths123 module!"))
+db.session.commit()
 # Multiple Maths Hard Summative
 
 Fill.query.delete()
@@ -140,5 +175,5 @@ db.session.commit()
 Assessment.query.delete()
 db.session.commit()
 db.session.add(Assessment(assessment_name="Test", module_code="Fruit123", admin_created=True,
-                          q1_type="Multiple", q1_id=1, q2_type="Fill", q2_id="1", q3_type="Fill", q3_id="3"))
+                          q1_type="Multiple", q1_id="1", q2_type="Fill", q2_id="1", q3_type="Fill", q3_id="3"))
 db.session.commit()
