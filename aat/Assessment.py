079e75aea97faeb472b3ca199f8087efbb959100
Tim Williams <williamst38@cardiff.ac.uk>
2021-05-18 16:05:40 +0100

    Revert "Merge branch 'tim2' into 'master'"
    This reverts merge request !81

--- aat/Assessment.py
+++ aat/Assessment.py
@@ -81,7 +81,7 @@ def testassess_route():
             if assessment.q2_type == "Multiple" :
                 answer_2=request.form.getlist("answer_2_multi")[0]
             else:
-                answer_2=form.answer_2_fill.data.lower()
+                answer_2=form.answer_2_fill.data
         
             if assessment.q3_type == "Multiple" :
                 answer_3=request.form.getlist("answer_3_multi")[0]
@@ -121,102 +121,15 @@ def testassess_route():
                                answer_1=answer_1, correct_1=correct_1,
                                answer_2=answer_2, correct_2=correct_2,
                                answer_3=answer_3, correct_3=correct_3,
-                               feedback_1 = assessment.q1_feedback,
-                               feedback_2 = assessment.q2_feedback,
-                               feedback_3 = assessment.q3_feedback,
-                               percentage_correct=percentage_correct, module_code=assessment.module_code,
-                               is_summative=assessment.is_summative, attempt_no=attempt, 
-                               assessment_name = assessment.assessment_name,
                                percentage_correct=percentage_correct, module_code=assessment.module_code,
                                is_summative=assessment.is_summative, attempt_no=attempt
-
                                )
+
             db.session.add(attempt)
             db.session.commit()
             flash("Assessment submitted")
             return redirect(url_for('studenthome'))
-            
 
-        return render_template('testassess.html', assessment=assessment,
-                               multiple_all=multiple_all, fill_all=fill_all,
-                               q1_multi=q1_multi, q1_fill=q1_fill, q1=q1,
-                               q2_multi=q2_multi, q2_fill=q2_fill, q2=q2,
-                               q3_multi=q3_multi, q3_fill=q3_fill, q3=q3,
-                               form=form
-                               )
-
-
-class Assessment_Form(FlaskForm):
-    question_1 = StringField('question-id-1', validators=[DataRequired()])
-    question_2 = StringField('question-id-2', validators=[DataRequired()])
-    question_3 = StringField('question-id-3', validators=[DataRequired()])
-    type_1 = StringField('question-type-1', validators=[DataRequired()])
-    type_2 = StringField('question-type-2', validators=[DataRequired()])
-    type_3 = StringField('question-type-3', validators=[DataRequired()])
-    module = StringField('module-code', validators=[DataRequired()])
-    submit = SubmitField('Save')
-    is_summative = StringField('assessment-type', validators=[DataRequired()])
-    feedback_date = DateField('Feedback-Date')
-
-
-def createassessment_route():
-    @app.route("/Create-Formative", methods=['GET', 'POST'])
-    def assessSubmit():
-        multiple_all = Multiple.query.all()
-        fill_all = Fill.query.all()
-        assessment_all = Assessment.query.all()
-        form = Assessment_Form()
-
-        if request.method == "POST":
-            print("Hi")
-            if request.form.get('selectedType') == "Formative":
-                form.is_summative.data = False
-            if request.form.get('selectedType') == "Summative":
-                form.is_summative.data = True
-
-            form.module.data = request.form.getlist('selectedModule')[0]
-            
-            if request.form.getlist("checked!"):
-                value = request.form.getlist('checked!')
-                print(value)
-                id_type = []
-                for i in range(len(value)):
-                    id_type.append(value[i].split(" "))
-
-                print(id_type)
-            createdAssessment = Assessment(q1_id=int(id_type[0][1]),
-                                           q2_id=int(id_type[1][1]),
-                                           q3_id=int(id_type[2][1]),
-                                           q1_type=id_type[0][0],
-                                           q2_type=id_type[1][0],
-                                           q3_type=id_type[2][0],
-                                           q1_feedback = id_type[0][2],
-                                           q2_feedback = id_type[1][2],
-                                           q3_feedback = id_type[2][2],
-                                           is_summative=form.is_summative.data,
-                                           assessment_name='Testing',
-                                           admin_created=True,
-                                           module_code=form.module.data
-                                           )
-            db.session.add(createdAssessment)
-            db.session.commit()
-            flash("Assessment added.")
-            return redirect(url_for('assessSubmit'))
-
-        return render_template("Create Formative.html", form=form, multiple_all=multiple_all, fill_all=fill_all, assessment_all=assessment_all)
-    
-    @app.route("/feedback", methods = ["GET", "POST"])
-    def viewfeedbacklist():
-        attempts = Attempts.query.filter_by(user_id=current_user.id).all()
-        
-
-        return render_template("Feedbacklist.html", attempts=attempts)
-    
-
-    @app.route("/feedback/<int:attempt_id>", methods = ["GET", "POST"])
-    def viewfeedback(attempt_id):
-        attempt = db.session.query(Attempts).get(attempt_id)
-        assessment = Assessment.query.get_or_404(attempt.assessment_id)
         return render_template('testassess.html', assessment=assessment,
                                multiple_all=multiple_all, fill_all=fill_all,
                                q1_multi=q1_multi, q1_fill=q1_fill, q1=q1,
@@ -285,45 +198,7 @@ def createassessment_route():
     
     @app.route("/feedback", methods = ["GET", "POST"])
     def viewfeedback():
-        attempts = Attempts.query.filter_by(user_id=current_user.id).all()
+        attempts = Attempts.query.filter_by(user_id=current_user.id).all
+        
 
-        q1_multi = Multiple.query.filter_by(id=assessment.q1_id).first()
-        q1_fill = db.session.query(Fill).get(assessment.q1_id)
-
-        if assessment.q1_type == "Multiple":
-            q1 = q1_multi.question
-            correct_1 = q1_multi.correct
-
-        else:
-            q1 = (q1_fill.question).replace(q1_fill.correct, "_______")
-            correct_1 = q1_fill.correct
-
-        q2_multi = Multiple.query.filter_by(id=assessment.q2_id).first()
-        q2_fill = db.session.query(Fill).get(assessment.q2_id)
-
-        if assessment.q2_type == "Multiple":
-            q2 = q2_multi.question
-            correct_2 = q2_multi.correct
-
-        else:
-            q2 = (q2_fill.question).replace(q2_fill.correct, "_______")
-            correct_2 = q2_fill.correct
-
-        q3_multi = Multiple.query.filter_by(id=assessment.q3_id).first()
-        q3_fill = db.session.query(Fill).get(assessment.q3_id)
-
-        if assessment.q3_type == "Multiple":
-            q3 = q3_multi.question
-            correct_3 = q3_multi.correct
-
-        else:
-            q3 = (q3_fill.question).replace(q3_fill.correct, "_______")
-            correct_3 = q3_fill.correct
-
-        return render_template("feedback.html", attempt = attempt,q1_multi=q1_multi, 
-                               q1_fill=q1_fill, q1=q1,
-                               q2_multi=q2_multi, q2_fill=q2_fill, q2=q2,
-                               q3_multi=q3_multi, q3_fill=q3_fill, q3=q3,
-                               correct_1 = correct_1, correct_2 = correct_2,
-                               correct_3 = correct_3)
         return render_template("Feedback.html", attempts=attempts)
