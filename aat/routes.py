3a3d972e1bf41497aa5b700f746ad80c6ef2e9da
Mike <64697794+M-Chapman@users.noreply.github.com>
2021-05-19 14:11:40 +0100

    added routing and assessment form submission for create summative page

--- aat/routes.py
+++ aat/routes.py
@@ -9,14 +9,14 @@ from aat.MultipleChoice import MultipleRoute, MultipleEditRoute
 from aat.Fill import filleditroute, fillblankroute, test_fill_route
 from aat.Assessment import Assessment_Form, createassessment_route, testassess_route
 
+
 @app.route("/c")
-
-
 @app.route("/Staff-Home", methods=["GET", 'POST'])
 def staffhome():
     assessment_all = Assessment.query.all()
 
     return render_template('Staff Home.html', assessment_all=assessment_all)
+
 
 @app.route("/DeleteAssessment/<int:assessment_id>", methods=['GET', 'POST'])
 def DeleteAssessment(assessment_id):
@@ -34,38 +34,43 @@ def DeleteAssessment(assessment_id):
 
     return render_template("AssessmentDelete.html", assessment=assessment)
 
+
 @app.route("/Student-Home", methods=["GET", 'POST'])
 def studenthome():
     assessment_all = Assessment.query.all()
     return render_template("Student Home.html", assessment_all=assessment_all)
 
+
 @app.route("/", methods=["GET", "POST"])
-@app.route("/login",methods=['GET','POST'])
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-  form = LoginForm()
-  if form.validate_on_submit():
-    user = User.query.filter_by(email=form.email.data).first()
-    if user is not None and user.verify_password(form.password.data):
-        login_user(user)
-        flash('Login successful!')
-        if user.is_admin:
-            return redirect(url_for('staffhome'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user)
+            flash('Login successful!')
+            if user.is_admin:
+                return redirect(url_for('staffhome'))
+            else:
+                return redirect(url_for("studenthome"))
         else:
-            return redirect(url_for("studenthome"))
-    else:
-        flash("Email or password is incorrect")
-  return render_template('login.html',title='Login',form=form)
+            flash("Email or password is incorrect")
+    return render_template('login.html', title='Login', form=form)
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, first_name=form.first_name.data, last_name=form.last_name.data, email=form.email.data, password=form.password.data)
+        user = User(username=form.username.data, first_name=form.first_name.data,
+                    last_name=form.last_name.data, email=form.email.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
         login_user(user)
         return redirect(url_for('staffhome'))
     return render_template('register.html', title='Register', form=form)
+
 
 @app.route("/logout")
 def logout():
@@ -73,7 +78,8 @@ def logout():
     flash('Logout successful!')
     return redirect(url_for('login'))
 
-#for fill in blank questions:
+
+# for fill in blank questions:
 fillblankroute()
 filleditroute()
 test_fill_route()
@@ -86,11 +92,54 @@ testassess_route()
 
 createassessment_route()
 
-# @app.route("/Create-Formative")
-# def createFormative():
-#     multiple_all = Multiple.query.all()
-#     fill_all = Fill.query.all()
-#     return render_template('Create Formative.html', multiple_all=multiple_all, fill_all=fill_all)
+
+@app.route("/Create-Summative", methods=['GET', 'POST'])
+def assessSubmit2():
+    multiple_all = Multiple.query.all()
+    fill_all = Fill.query.all()
+    assessment_all = Assessment.query.all()
+    form = Assessment_Form()
+
+    if request.method == "POST":
+        print("Hi")
+        if request.form.get('selectedType') == "Formative":
+            form.is_summative.data = False
+        if request.form.get('selectedType') == "Summative":
+            form.is_summative.data = True
+
+        form.module_code.data = request.form.getlist('selectedModule')[0]
+
+        if request.form.getlist("checked!"):
+            value = request.form.getlist('checked!')
+            id_type = []
+            for i in range(len(value)):
+                id_type.append(value[i].split(" ", 2))
+            print("tim")
+            print(id_type)
+            print(id_type[0][2])
+        createdAssessment = Assessment(q1_id=int(id_type[0][1]),
+                                           q2_id=int(id_type[1][1]),
+                                           q3_id=int(id_type[2][1]),
+                                           q1_type=id_type[0][0],
+                                           q2_type=id_type[1][0],
+                                           q3_type=id_type[2][0],
+                                           q1_feedback=id_type[0][2],
+                                           q2_feedback=id_type[1][2],
+                                           q3_feedback=id_type[2][2],
+                                           is_summative=form.is_summative.data,
+                                           assessment_name=form.assessment_name.data,
+                                           admin_created=True,
+                                           module_code=form.module_code.data
+                                           )
+        db.session.add(createdAssessment)
+        db.session.commit()
+        flash("Assessment added.")
+        return redirect(url_for('assessSubmit2'))
+
+    return render_template("Create Summative.html", form=form,
+                               multiple_all=multiple_all, fill_all=fill_all,
+                               assessment_all=assessment_all)
+
 
 @app.route("/Create-Summative")
 def createSummative():
@@ -98,30 +147,37 @@ def createSummative():
     fill_all = Fill.query.all()
     return render_template('Create Summative.html', multiple_all=multiple_all, fill_all=fill_all)
 
+
 @app.route("/Student-View-Progress")
 def studentViewProgress():
     return render_template('Student View Progress.html')
+
 
 @app.route("/Student-View-Attempts")
 def studentViewAttempts():
 
     return render_template('Student View Attempts.html')
 
+
 @app.route("/View-Quiz-Attempt")
 def viewQuizAttempt():
     return render_template('View Attempt.html')
+
 
 @app.route("/Generate-Quiz")
 def generateQuiz():
     return render_template('Generate Quiz.html')
 
+
 @app.route("/Attempt-Assessment")
 def attemptAssessment():
     return render_template('Attempt Assessment.html')
 
+
 @app.route("/View-Cohort")
 def viewCohort():
     return render_template('View Cohort.html')
+
 
 @app.route("/View-Student")
 def viewStudent():
@@ -137,6 +193,7 @@ def createQuestion():
 def previewAssessment():
     return render_template('Preview Assessment.html')
 
+
 @app.route("/Submit-Assessment")
 def submitAssessment():
     return render_template('Submit Assessment.html')
@@ -145,17 +202,20 @@ def submitAssessment():
 @app.route('/View-Student-Number')
 def viewStudentNumber():
     post = User.query(db.func.count(User.is_admin == False)).all()
-    return render_template('View Student Number.html',post=post)
+    return render_template('View Student Number.html', post=post)
+
 
 @app.route('/View-Student-List')
 def viewStudentList():
     post = User.query.all()
-    return render_template('View Student List.html',post=post)
+    return render_template('View Student List.html', post=post)
+
 
 @app.route('/Search-Student')
 def searchStudent():
     #post = User.query.filter(User.id == ('%{keyword}%'.format(keyword=request.form.get('search_input')))).all()
     return render_template("View Student Search.html")
+
 
 @app.route('/View-Attempts')
 def viewAttempts():
@@ -168,7 +228,7 @@ def EditAssessment(assessment_id):
     multiple_all = Multiple.query.all()
     fill_all = Fill.query.all()
     assessment = db.session.query(Assessment).get(assessment_id)
-    form = Assessment_Form(formdata=request.form, obj = assessment)
+    form = Assessment_Form(formdata=request.form, obj=assessment)
     if form.validate_on_submit():
         if request.form.getlist("checked!"):
             value = request.form.getlist('checked!')
@@ -180,25 +240,24 @@ def EditAssessment(assessment_id):
             assessType = True
         else:
             assessType = False
-    
+
         db.session.delete(assessment)
-        assessment_edit = Assessment(id = assessment_id,
-                        q1_id=int(id_type[0][1]),
-                        q2_id=int(id_type[1][1]),
-                        q3_id=int(id_type[2][1]),
-                        q1_type=id_type[0][0],
-                        q2_type=id_type[1][0],
-                        q3_type=id_type[2][0],
-                        q1_feedback = id_type[0][2],
-                        q2_feedback = id_type[1][2],
-                        q3_feedback = id_type[2][2],
-                        module_code=form.module_code.data, # Needs changing 
-                        is_summative=assessType,
-                        assessment_name=form.assessment_name.data
-                        )
+        assessment_edit = Assessment(id=assessment_id,
+                                     q1_id=int(id_type[0][1]),
+                                     q2_id=int(id_type[1][1]),
+                                     q3_id=int(id_type[2][1]),
+                                     q1_type=id_type[0][0],
+                                     q2_type=id_type[1][0],
+                                     q3_type=id_type[2][0],
+            q1_feedback=id_type[0][2],
+            q2_feedback=id_type[1][2],
+            q3_feedback=id_type[2][2],
+            module_code=form.module_code.data,  # Needs changing
+            is_summative=assessType,
+            assessment_name=form.assessment_name.data
+        )
         db.session.add(assessment_edit)
         db.session.commit()
         flash("edited successfully")
         return redirect(url_for('staffhome'))
     return render_template('EditAssessment.html', assessment=assessment, form=form, multiple_all=multiple_all, fill_all=fill_all)
-
